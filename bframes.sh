5d9fca2ae37c846a138f3b93c4cf98661333e076
Rob Nugen <rob@robnugen.com>
2019-05-10 23:08:24 +0900

    bframes.sh

--- bframes.sh
+++ bframes.sh
@@ -2,8 +2,8 @@
 # 
 # set these values
 
-frame_source_dir=~/mt3/00010_002.dgn/00010_002_Take_09/00010_002_09_X1/
+frame_source_dir=~/mt3/00010_002.dgn/00010_002_Take_10/00010_002_10_X1/
 
-scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/2018
+scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/2019
 
 ssh b.rn '~/scripts/create_thumbs.pl'
