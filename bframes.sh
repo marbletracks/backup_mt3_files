9a31271ededb9d76f3d6acdad7905e9ab4601114
Rob Nugen <rob@robnugen.com>
2020-12-21 17:28:54 +0900

    Update bframes to this year and stuff
    a little late, given that 2021 is coming soon

--- bframes.sh
+++ bframes.sh
@@ -2,8 +2,8 @@
 # 
 # set these values
 
-frame_source_dir=~/mt3/00010_002.dgn/00010_002_Take_10/00010_002_10_X1/
+frame_source_dir=~/mt3/00010_002.dgn/00010_002_Take_11/00010_002_11_X1/
 
-scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/2019
+scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/2020
 
 ssh b.rn '~/scripts/create_thumbs.pl'
