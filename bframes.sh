8531703e05b678caa40bd282d2b678a67ab3c3a8
Rob Nugen <rob@robnugen.com>
2021-01-25 11:13:53 +0900

    Update bframes to current year

--- bframes.sh
+++ bframes.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
-# 
+#
 # set these values
+
+THISYEAR=$(date +'%Y')
 
 frame_source_dir=~/mt3/00010_002.dgn/00010_002_Take_11/00010_002_11_X1/
 
-scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/2020
+scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/$THISYEAR
 
 ssh b.rn '~/scripts/create_thumbs.pl'
