0ca72d859e8ac25b05d0387c567076ca4984f77f
Rob Nugen <rob@robnugen.com>
2021-10-12 08:20:20 +0900

    Speed up thumbnail creation

--- bframes.sh
+++ bframes.sh
@@ -6,6 +6,6 @@ THISYEAR=$(date +'%Y')
 
 frame_source_dir=~/mt3/00010_002.dgn/00010_002_Take_11/00010_002_11_X1/
 
-scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:mt3/frames/$THISYEAR
+scp `find $frame_source_dir*.jpg -type f -mtime -1` b.rn:b.robnugen.com/art/marble_track_3/frames/$THISYEAR
 
-ssh b.rn '~/scripts/create_thumbs.pl'
+ssh b.rn "~/scripts/create_thumbs.pl /home/thundergoblin/b.robnugen.com/art/marble_track_3/frames/$THISYEAR"
